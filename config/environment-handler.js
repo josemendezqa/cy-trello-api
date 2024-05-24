19087a83c2335c439f57e2103a4fd7b78327f16e
Jose Mendez <josemendez.qaengineer@gmail.com>
2024-05-24 15:52:33 -0600

    passing email and password locators from file

--- config/environment-handler.js
+++ config/environment-handler.js
@@ -1,8 +1,10 @@
 const app = require('./app.json')
+const dev = require('./dev.json')
 
 exports.getEnv = function (envSelected) {
 	let environmentList = {
 		app: app,
+		dev:dev
 	}
-	return environmentList[envSelected] || environmentList.app
+	return environmentList[envSelected] || environmentList.dev
 }
--- cypress/e2e/ui/tests/login/pages/login-page.js
+++ cypress/e2e/ui/tests/login/pages/login-page.js
@@ -9,7 +9,7 @@ export class LoginPage {
 		this.LoginButton = new ButtonComponent(LoginLocators.LOGIN_BUTTON)
 		this.GoogleButton = new ButtonComponent(LoginLocators.GOOGLE_BUTTON)
 		this.EmailInput = new InputComponent(LoginLocators.EMAIL_INPUT)
-		this.PasswordInput
+		this.PasswordInput = new InputComponent(LoginLocators.PASSWORD_INPUT)
 	}
 
 	navigateToLoginPage() {
@@ -20,10 +20,16 @@ export class LoginPage {
 	login() {
 		//cy.visit('https://trello.com/')
 		//cy.get(loginButtonLocator).click()
-		cy.origin('https://id.atlassian.com', () => {
-			cy.get('[data-testid="username"]').type('josemendez.qaengineer@gmail.com')
+		const emailInputSelector = this.EmailInput.locator;
+        const passwordInputSelector = this.PasswordInput.locator;
+
+		console.log(emailInputSelector);
+		console.log(passwordInputSelector);
+
+		cy.origin('https://id.atlassian.com', { args: { emailInputSelector, passwordInputSelector } }, ({ emailInputSelector, passwordInputSelector }) => {
+			cy.get(emailInputSelector).type('josemendez.qaengineer@gmail.com')
 			cy.contains('Continue').click()
-			cy.get('[data-testid="password"]').type('123Queso!')
+			cy.get(passwordInputSelector).type('123Queso!')
 			cy.get('#login-submit').click({ force: true })
 		})
 	}
